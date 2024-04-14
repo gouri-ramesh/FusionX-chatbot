be7b70990827f254c3a8fc0a5430998ce63b49fb
Gouri Ramesh <gouri.ramesh07@gmail.com>
2024-04-14 18:07:16 +0530

    chatbot icon removed

--- standalone-frontend/app.js
+++ standalone-frontend/app.js
@@ -1,49 +1,46 @@
 class Chatbox {
     constructor() {
         this.args = {
-            openButton: document.querySelector('.chatbox__button'),
             chatBox: document.querySelector('.chatbox__support'),
             sendButton: document.querySelector('.send__button')
         }
 
-        this.state = false;
+        this.state = true; // Set initial state to open
         this.messages = [];
+
+        // Display the chatbox immediately
+        this.toggleState(this.args.chatBox);
     }
 
     display() {
-        const {openButton, chatBox, sendButton} = this.args;
+        const { chatBox, sendButton } = this.args;
 
-        openButton.addEventListener('click', () => this.toggleState(chatBox))
-
-        sendButton.addEventListener('click', () => this.onSendButton(chatBox))
+        sendButton.addEventListener('click', () => this.onSendButton(chatBox));
 
         const node = chatBox.querySelector('input');
         node.addEventListener("keyup", ({key}) => {
             if (key === "Enter") {
-                this.onSendButton(chatBox)
+                this.onSendButton(chatBox);
             }
-        })
+        });
     }
 
     toggleState(chatbox) {
-        this.state = !this.state;
+        // You might want to remove the toggle functionality as the chatbox is always open now
+        // this.state = !this.state;
 
-        // show or hides the box
-        if(this.state) {
-            chatbox.classList.add('chatbox--active')
-        } else {
-            chatbox.classList.remove('chatbox--active')
-        }
+        // show or hide the box
+        chatbox.classList.add('chatbox--active');
     }
 
     onSendButton(chatbox) {
         var textField = chatbox.querySelector('input');
-        let text1 = textField.value
+        let text1 = textField.value;
         if (text1 === "") {
             return;
         }
 
-        let msg1 = { name: "User", message: text1 }
+        let msg1 = { name: "User", message: text1 };
         this.messages.push(msg1);
 
         fetch('http://127.0.0.1:5000/predict', {
@@ -58,26 +55,23 @@ class Chatbox {
           .then(r => {
             let msg2 = { name: "Sam", message: r.answer };
             this.messages.push(msg2);
-            this.updateChatText(chatbox)
-            textField.value = ''
+            this.updateChatText(chatbox);
+            textField.value = '';
 
         }).catch((error) => {
             console.error('Error:', error);
-            this.updateChatText(chatbox)
-            textField.value = ''
+            this.updateChatText(chatbox);
+            textField.value = '';
           });
     }
 
     updateChatText(chatbox) {
         var html = '';
         this.messages.slice().reverse().forEach(function(item, index) {
-            if (item.name === "Sam")
-            {
-                html += '<div class="messages__item messages__item--visitor">' + item.message + '</div>'
-            }
-            else
-            {
-                html += '<div class="messages__item messages__item--operator">' + item.message + '</div>'
+            if (item.name === "Sam") {
+                html += '<div class="messages__item messages__item--visitor">' + item.message + '</div>';
+            } else {
+                html += '<div class="messages__item messages__item--operator">' + item.message + '</div>';
             }
           });
 
@@ -85,7 +79,6 @@ class Chatbox {
         chatmessage.innerHTML = html;
     }
 }
-
 
 const chatbox = new Chatbox();
 chatbox.display();
